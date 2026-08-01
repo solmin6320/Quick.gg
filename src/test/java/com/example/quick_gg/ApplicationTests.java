610f78d7e3351ee38b6ae9460ea07337f172e2c2
kim090803 <2025230@sungil-i.kr>
2026-08-01 22:57:31 +0900

    프로젝트 기본 환경설정 추가(gradle)

---
+++ src/test/java/com/example/quick_gg/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick_gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
