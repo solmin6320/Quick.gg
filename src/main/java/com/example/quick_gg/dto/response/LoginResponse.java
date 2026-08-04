ae080d71ac84e327233a263a5d7d384af479ec98
solmin6320 <solmin6320@gmail.com>
2026-08-04 22:24:47 +0900

    회원가입, 로그인 응답 dto 객체 작성

---
+++ src/main/java/com/example/quick_gg/dto/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.example.quick_gg.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    // 로그인한 학생의 PK
+    private Long id;
+
+    // API 요청 인증용 JWT
+    private String accessToken;
+
+    // 액세스 토큰 재발급용
+    private String refreshToken;
+
+    // 로그인 시각
+    private LocalDateTime loginAt;
+}
