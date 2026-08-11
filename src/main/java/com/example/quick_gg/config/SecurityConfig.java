d90dfd01c434af4b777edb02d97c0feaa0f779bd
solmin6320 <solmin6320@gmail.com>
2026-08-11 20:29:07 +0900

    SecurityConfig 구성 (JWT 인증 필터 체인 적용)

--- src/main/java/com/example/quick_gg/config/SecurityConfig.java
+++ src/main/java/com/example/quick_gg/config/SecurityConfig.java
@@ -3,23 +3,65 @@ package com.example.quick_gg.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.quick_gg.security.JwtAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     // BCrypt를 사용하는 PasswordEncoder 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    // 인증 매니저가 추가될 자리
 
+    // 로그인 시 아이디, 비밀번호 인증을 처리
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+    throws Exception {
+        return configuration.getAuthenticationManager();
+    }
     
     
-    // 시큐리티 필터 체인이 추가될 자리
+    // 보안 규칙(필터 체인)을 정의
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // CSRF 방어 비활성화(세션/쿠키 사용 X)
+                .csrf(csrf -> csrf.disable())
+
+                // 세션 저장소를 사용하지 않고, 매 요청마다 JWT로만 인증
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // 경로별 인가 규칙 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/signup", "/auth/login")
+                        .permitAll()
+                        .anyRequest().authenticated() // 회원가입, 로그인 제외 모든 경로는 인증된 사용자만 접근 가능
+                )
+                // Jwt 인증 필터를 기본 필터 보다 먼저 실행
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return httpSecurity.build();
+
+
+    }
+
 }
