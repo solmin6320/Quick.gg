20e94b95ff6dff71c937bba583e54a579390d39f
solmin6320 <solmin6320@gmail.com>
2026-08-17 13:03:27 +0900

    SecurityConfig CORS 정책 추가

--- src/main/java/com/example/quick_gg/config/SecurityConfig.java
+++ src/main/java/com/example/quick_gg/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.quick_gg.security.JwtAuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -40,8 +45,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                // CSRF 방어 비활성화(세션/쿠키 사용 X)
+                // CORS 추가
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // CSRF 방어 비활성화(쿠키만으로 인증이 완성되지 않음)
                 .csrf(csrf -> csrf.disable())
+
 
                 // 세션 저장소를 사용하지 않고, 매 요청마다 JWT로만 인증
                 .sessionManagement(session ->
@@ -62,6 +70,23 @@ public class SecurityConfig {
         return httpSecurity.build();
 
 
+    }
+
+    // CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+         CorsConfiguration configuration = new CorsConfiguration();
+
+         configuration.setAllowedOrigins(List.of("https://quick-gg-front.com")); // 프론트 도메인(이미 이름을 정했음)
+         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할  HTTP 메서드 목록
+         configuration.setAllowedHeaders(List.of("*")); // 요청 시 허용할 헤더
+         configuration.setAllowCredentials(true); // 쿠키를 주고받을 수 있도록 허용
+         configuration.setMaxAge(3600L); // 요청 결과를 브라우저가 개시(초)
+
+        // 위 설정을 모든 경로에 동일하게 적용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
